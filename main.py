083a8dbf7fe51c903e678ee01cc81449cdbf298a
Igor Tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-09-17 16:02:40 +0400

    refactor: delete unused files

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.parser import fetch_products
-from src.quality_check import check_quality
-
-if __name__ == "__main__":
-    products = fetch_products()
-    if check_quality(products):
-        print("Данные прошли проверку качества.")
-    else:
-        print("Данные не прошли проверку качества.")
